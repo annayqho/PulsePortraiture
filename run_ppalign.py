26d6d13a1cb23bae412ea4cbe446a468a8b32571
Anna Ho <annaho@mpia.de>
2015-09-30 21:02:07 -0400

    first commit

---
+++ run_ppalign.py
@@ -0,0 +1,11 @@
+import glob
+from ppalign import align_archive
+
+filenames = glob.glob("/nimrod1/GBT/Ter5/GUPPI/Sband_tscr/*Ter5O*.zap")
+filenames = ['/nimrod1/GBT/Ter5/GUPPI/Sband_tscr/*Ter5O*120415*.zap']
+template = "/nimrod1/GBT/Ter5/GUPPI/PSRs/templates/Ter5O_256_gaussians.template"
+outfiles = [f+"_aligned" for f in filenames]
+
+for f,o in zip(filenames, outfiles):
+    align_archive(f, template, o, tfac=2)
+
