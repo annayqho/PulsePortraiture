cf27ad982e871c7d6cc19a0dac4f9fe171d1a75c
Anna Ho <annaho@mpia.de>
2015-10-01 14:29:02 -0400

    run all files

--- run_ppalign.py
+++ run_ppalign.py
@@ -2,10 +2,9 @@ import glob
 from ppalign import align_archive
 
 filenames = glob.glob("/nimrod1/GBT/Ter5/GUPPI/Sband_tscr/*Ter5O*.zap")
-filenames = ['/nimrod1/GBT/Ter5/GUPPI/Sband_tscr/*Ter5O*120415*.zap']
 template = "/nimrod1/GBT/Ter5/GUPPI/PSRs/templates/Ter5O_256_gaussians.template"
 outfiles = [f+"_aligned" for f in filenames]
 
 for f,o in zip(filenames, outfiles):
-    align_archive(f, template, o, tfac=2)
+    align_archive(f, template, o, tfac=1)
 
